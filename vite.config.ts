ae1d8fef7780f103e8ec632f116839234babd98c
Brainyark <antonywaithaka80@gmail.com>
2025-03-02 23:17:24 -1000

    Updated Vite config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Brainyark-premium-jacket-ecommerce/', // Required for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
+  }
 });
